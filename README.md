9f33b58065f6e79206e0fa4ceb6896611927931f
Ben Attenborough <battenborough@gmail.com>
2021-10-16 22:25:28 +0100

    Add ability to render map

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ The intention is to create a tool for making Zelda like games.
 A map will be stored to represent a world. Each element of the map will represent a tile.
 The dev can input a table with the map information and a spritesheet and the utility will draw the map.
 Extending on this the map could extend beyond the edges of the screen and be scrollable.
+
+Will need some sort of configuration.
+I'm thinking the tilemap2d class will take a table of values on initialisation. The table will contain:
+* Map array - a two-dimensional array which represents the map to be rendered
+* Spritemap - a spritemap which is a png representing all the tiles
+* Tile size - A width and height for each tile. The engine will cut out tiles from the sprite map depending on these variables
--- tilemap2d.lua
+++ tilemap2d.lua
@@ -1,12 +1,35 @@
 TileMap2d = Class{}
 
-function TileMap2d:init(playX,playY,playWidth,playHeight)
-    self.playX = playX
-    self.playY = playY
-    self.playWidth = playWidth
-    self.playHeight = playHeight
+function TileMap2d:init(config)
+    self.map = config['map']
+    self.tileSheet = love.graphics.newImage(config['spriteSheet'])
+    self.tw = config['spriteSize']['width']
+    self.th = config['spriteSize']['height']
+    self.tiles = {}
+    for i=1,config['spriteCount']
+    do
+        self.tiles[i] = love.graphics.newQuad( (i - 1) * self.tw , 0, self.tw, self.th, self.tileSheet )
+    end
 end
 
-function TileMap2d:draw() 
-    love.graphics.rectangle("fill",self.playX,self.playY,self.playWidth,self.playHeight)
+function TileMap2d:draw(offsetx, offsety, width, height) 
+    originalOffsetX = offsetx
+    tile = self.tiles[1]
+    for x = 1,table.maxn(self.map)
+    do
+        for y = 1,table.maxn(self.map[1])
+        do
+            tile = self.tiles[self.map[x][y]]
+            love.graphics.draw(self.tileSheet, tile, offsetx, offsety)
+            r, g, b, a = love.graphics.getColor( )
+            -- if x == self.hoveredTile[1] and y == self.hoveredTile[2] then
+            --     r, g, b, a = love.graphics.getColor( )
+            --     love.graphics.setColor(1,1,1)
+            --     love.graphics.rectangle("line", offsetx, offsety, self.tileWidth, self.tileHeight)
+            -- end
+            offsetx = offsetx + self.tw
+        end
+        offsetx = originalOffsetX
+        offsety = offsety + self.th
+    end
 end
