471bc0dc3a705c1c2a340448cd7c74d8be8b4992
Ben Attenborough <battenborough@gmail.com>
2021-10-31 17:02:02 +0000

    WIP Seperate out map editor into it's own file

---
+++ MapEditor.lua
@@ -0,0 +1,28 @@
+MapEditor = Class{}
+
+require 'objects.Button'
+
+function MapEditor:init(config)
+    self.saveButton = Button(20,200,100,14,'Save',self.saveHandler)
+    self.loadButton = Button(20,225,100,14,'Load',self.loadHandler)
+end
+
+function MapEditor:render()
+    self.saveButton:draw()
+    self.loadButton:draw()
+end
+
+function MapEditor:saveHandler()
+    print("Save clicked")
+    Tilemap2d:save()
+end
+
+function MapEditor:loadHandler()
+    print("Load clicked")
+    Tilemap2d:load()
+end
+
+function MapEditor:mousereleased(x, y, button)
+    self.saveButton:mousereleased(x, y, button)
+    self.loadButton:mousereleased(x, y, button)
+end
